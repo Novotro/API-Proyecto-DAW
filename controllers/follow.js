a952093f4f34f9454898d7a53a0be12954856017
Novotro <rubendr91@gmail.com>
2018-05-11 17:23:38 +0200

    Listado de usuarios que nos siguen (Sin acabar)

--- controllers/follow.js
+++ controllers/follow.js
@@ -68,21 +68,45 @@ function getFollowingUsers(req,res){
   });
 
 }
-//Prueba de commits
 
 
 
+function getFollowedUsers(req, res){
+  var userId = req.user.sub;
+
+  if(req.params.id && req.params.page){
+    userId = req.params.id
+  }
+
+  var page = 1;
+
+  if(req.params.page){
+    page = req.params.page;
+  }else{
+    page = req.params.id;
+  }
+
+  var itemsPerPage = 4;
+
+  Follow.find({followed:userId}).populate({path: 'followed'}).paginate(page, itemsPerPage, (err,follows, total) =>{
+      if(err) return res.status(500).send({message: 'Error en el servidor'});
+
+      if(!follows) return res.status(404).send({message: 'No te sigue ningun usuario'});
+
+      return  res.status(200).send({
+          total: total,
+          pages: Math.ceil(total/itemsPerPage),
+          follows
+       });
+  });
 
 
 
-
-
-
-
-
+}
 
 module.exports={
   saveFollow,
   deleteFollow,
-  getFollowingUsers
+  getFollowingUsers,
+  getFollowedUsers
 }
