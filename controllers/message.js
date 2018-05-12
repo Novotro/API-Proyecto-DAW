d43365fb0fda737bee96ed9fcae44f22d28ed1ec
Novotro <rubendr91@gmail.com>
2018-05-13 00:58:56 +0200

    Envio de mensaje directo

--- controllers/message.js
+++ controllers/message.js
@@ -7,11 +7,29 @@ var User = require('../models/user');
 var Follow = require('../models/follow');
 var Message = require('../models/message');
 
-function probando(req,res){
-  res.status(200).send({message: 'Hola que tal'});
+
+//Enviar mensaje
+function saveMessage(req,res){
+  var params = req.body;
+
+  if(!params.text || !params.receiver){return res.status(200).send({message: 'Envia los datos necesarios'})  }
+
+  var message = new Message();
+
+  message.emitter= req.user.sub;
+  message.receiver = params.receiver;
+  message.text = params.text;
+  message.created_at = moment().unix();
+
+  message.save((err, messageStored) =>{
+      if(err)return res.status(500).send({message: 'Error en la peticion'});
+      if(!messageStored)return res.status(500).send({message: 'Error al enviar el mensaje'});
+
+      return res.status(200).send({message: messageStored});
+  });
 
 }
 
 module.exports= {
-  probando  
+  saveMessage
 }
