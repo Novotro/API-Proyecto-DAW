b94f34684ef1168e079e2eb0edbe4b14a1c66271
Novotro <rubendr91@gmail.com>
2018-05-12 17:52:14 +0200

    Publicaciones paginadas con timeline

--- controllers/publication.js
+++ controllers/publication.js
@@ -12,7 +12,7 @@ var Follow = require('../models/follow');
 function probando(req,res){
   res.status(200).send({message: "hola desde el controlador de publicaciones"});
 }
-
+//Guardar publicaciones
 function savePublication(req,res){
   var params = req.body;
 
@@ -33,11 +33,45 @@ function savePublication(req,res){
 
   });
 
+}
+
+//Devolver las publicaciones de todos los usuarios que sigo
+function getPublications(req,res){
+  var page=1;
+  if(req.params.page){
+    page = req.params.page;
+  }
+  var itemsPerPage= 4;
+
+  Follow.find({user: req.user.sub}).populate('followed').exec((err,follows)=>{
+    if(err) return res.status(500).send({message: 'Error al devolver el seguimiento'});
+
+    var follows_clean = [];
+
+    follows.forEach((follow)=>{
+      follows_clean.push(follow.followed);
+    });
+
+    //Busca todos los usuarios que esten dentro de follows clean
+    Publication.find({user: {$in: follows_clean}}).sort('-created_at').populate('user').paginate(page,itemsPerPage, (err,publications,total)=>{
+      if(err) return res.status(500).send({message: 'Error al devolver promublicaciones'});
+
+      if(!publications) return res.status(404).send({message: 'No hay publicaciones'});
+
+      return res.status(200).send({
+        total_items: total,
+        pages: Math.ceil(total/itemsPerPage),
+        page: page,
+        publications
+      });
+    });
+  });
 
 }
 
 
 module.exports={
   probando,
-  savePublication
+  savePublication,
+  getPublications
 }
