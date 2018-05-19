33ba7758cbef7530483d252d4f69abef7d17b883
Novotro <rubendr91@gmail.com>
2018-05-19 16:37:18 +0200

    Creacion de metodo para devolver publicaciones de un solo usuario

--- controllers/publication.js
+++ controllers/publication.js
@@ -71,6 +71,35 @@ function getPublications(req,res){
   });
 }
 
+//Devolver las publicaciones de un usuario en concreto
+function getPublicationsUser(req,res){
+  var page=1;
+  if(req.params.page){
+    page = req.params.page;
+  }
+  var itemsPerPage= 4;
+
+  var user= req.user.sub;
+  if(req.params.user){
+      user = req.params.user;
+  }
+
+    Publication.find({user: user}).sort('-created_at').populate('user').paginate(page,itemsPerPage, (err,publications,total)=>{
+      if(err) return res.status(500).send({message: 'Error al devolver publicaciones'});
+
+      if(!publications) return res.status(404).send({message: 'No hay publicaciones'});
+
+      return res.status(200).send({
+        total_items: total,
+        pages: Math.ceil(total/itemsPerPage),
+        page: page,
+        items_per_page: itemsPerPage,
+        publications
+      });
+
+    });
+}
+
 //Obtener una publicacion segun su id
 function getPublication(req,res){
   var publicationId = req.params.id;
@@ -104,5 +133,6 @@ module.exports={
   savePublication,
   getPublications,
   getPublication,
-  deletePublication
+  deletePublication,
+  getPublicationsUser
 }
