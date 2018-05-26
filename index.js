387069df497bc237b08a70aabecee41bae541afc
v666 <v666@1nterc3pt0r.org>
2018-05-26 19:12:31 +0200

    sdsd

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var port = 3800; //Puerto en el que trabajaremos
 
 //Conexion base de datos
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/curso_mean_social')
+mongoose.connect('mongodb://localhost:27017/credSint')
           .then(() => {
               console.log("Conexion a la base de datos exitosa");
 
               //Crear servidor
               app.listen(port,() =>{
-                    console.log("Servidor corriendo en http://localhost:3800");                
+                    console.log("Servidor corriendo en http://localhost:3800");
               });
 
           })
